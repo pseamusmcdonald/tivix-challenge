e28080cb70a1615a5ec9190ac2f4348dbec369bc
Seamus McDonald <pseamus1212@gmail.com>
2021-12-04 16:41:48 -0500

    updated gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,14 +13,6 @@ module.exports = {
     	"gatsby-plugin-image",
     	"gatsby-plugin-sharp",
     	"gatsby-transformer-sharp",
-    	{
-      		resolve: "gatsby-source-filesystem",
-      		options: {
-        		name: "images",
-        		path: "./src/assets/images/",
-      		},
-      		__key: "images",
-		},
 		{
 			resolve: 'gatsby-source-prismic',
 			options: {
